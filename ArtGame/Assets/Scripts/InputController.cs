9cea848b70373e38b772290246a08ad600a6ba0a
Simon <666ohyeah999@gmail.com>
2019-10-09 15:02:37 +0200

    Polish + fluid sphere + particles

---
+++ ArtGame/Assets/Scripts/InputController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MidiJack;
+using System;
+
+
+public class InputController : MonoBehaviour
+{
+    public CubeSphere planet;
+    public MeshDeformer md;
+    public MeshDeformerInput mdi;
+
+    void Update()
+    {
+        
+        if(MidiMaster.GetKeyDown(Convert.ToInt32("3C", 16))) //format "Quand la touche est enfoncée"
+        {
+            md.springForce = 2;
+            mdi.force = 40;
+        }
+
+        if (MidiMaster.GetKeyDown(Convert.ToInt32("3E", 16)))
+        {
+            md.springForce = 5;
+            mdi.force = 30;
+        }
+
+        if (MidiMaster.GetKey(Convert.ToInt32("40", 16)) > 0)   //format "Tant que la touche est enfoncée"
+        {
+
+        }
+
+        if (MidiMaster.GetKeyUp(Convert.ToInt32("41", 16))) //format "Quand la touche est relachée"
+        {
+
+        }
+
+        if (MidiMaster.GetKeyDown(Convert.ToInt32("43", 16)))
+        {
+
+        }
+
+        if (MidiMaster.GetKeyDown(Convert.ToInt32("45", 16)))
+        {
+
+        }
+
+        if (MidiMaster.GetKeyDown(Convert.ToInt32("47", 16)))
+        {
+
+        }
+
+        if (MidiMaster.GetKeyDown(Convert.ToInt32("48", 16)))
+        {
+
+        }
+
+        if (MidiMaster.GetKeyDown(Convert.ToInt32("4A", 16)))
+        {
+
+        }
+
+        if (MidiMaster.GetKeyDown(Convert.ToInt32("4C", 16)))
+        {
+
+        }
+
+        if (MidiMaster.GetKeyDown(Convert.ToInt32("4D", 16)))
+        {
+
+        }
+
+        if (MidiMaster.GetKeyDown(Convert.ToInt32("4F", 16)))
+        {
+
+        }
+    }
+}
---
+++ ArtGame/Assets/SlowMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowMove : MonoBehaviour
+{
+    private float counter = 0f;
+    private Vector3 begin;
+
+    private void Start()
+    {
+        begin = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter += Time.deltaTime;
+        transform.position = begin + Vector3.forward * Curves.Sinus(counter / 4) * 0.5f;
+    }
+}
